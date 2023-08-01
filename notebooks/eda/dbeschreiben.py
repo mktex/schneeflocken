fbe29d1f2a8475ad3c6ddfe7c907bbbfe74c215e
mktech <9596910+mktex@users.noreply.github.com>
2023-08-01 21:53:06 +0200

    erneut

---
+++ notebooks/eda/dbeschreiben.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+
+import seaborn as sbn
+import numpy as np
+import pandas as pd
+from functools import reduce
+from contextlib import contextmanager
+
+
+@contextmanager
+def SuppressPandasWarning():
+    with pd.option_context("mode.chained_assignment", None):
+        yield
+
+
+# Ergibt die Anzahl oder Prozent der Nullen in einer Spalte
+xNullen = lambda df, xcol: df[df[xcol].isnull()].shape[0]
+xProzentNullen = lambda df, xcol: df[df[xcol].isnull()].shape[0] / df.shape[0]
+xNullenInDF = lambda xdf: pd.DataFrame([(xcol, xNullen(xdf, xcol)) for xcol in xdf.columns])
+xProzentNullenInDF = lambda xdf: pd.DataFrame([(xcol, xProzentNullen(xdf, xcol)) for xcol in xdf.columns])
+
+# Ergibt die Durchschnittswerte einer kontinuierlicher Variable pro Wert aus einer Kategorialer Variable
+xDurchschnittswertProKateg = lambda df, xkateg, xwert: df[[xkateg, xwert]] \
+    .groupby(xkateg).mean().sort_values(by=xwert)
+
+
+def kateg_werte_liste(xdf_input, xcol, sep=None):
+    """ Ergibt die Liste der Werte in kategorialer Variable
+        Bleibt der sep None, dann sind die Werte nichts anderes als ein set(xL)
+    """
+    xL = list(map(lambda x: str(x), xdf_input[xcol].values.tolist()))
+    xL = list(filter(lambda x: x is not np.nan, xL))
+    if sep is not None:
+        xL = list(map(lambda x: list(set(x.split(sep))), xL))
+        xL = list(reduce(lambda a, b: a + b, xL))
+    xL = [str(t).strip() for t in xL]
+    return xL
+
+
+def frequenz_werte(xdf_input, xcol="CousinEducation", prozente=False, sep=None):
+    """ Anzahl der Datensätze mit xcol == {Wert} ODER Wert in xcol.split(sep)
+    """
+    df = xdf_input.copy()
+    xl = kateg_werte_liste(df, xcol, sep=sep)
+    xs = pd.DataFrame({xcol: xl})
+    xs['id'] = xs.index.values.tolist()
+    xres = xs.groupby(xcol, as_index=True).count()
+    xres = xres.sort_values(by="id", ascending=False)
+    if prozente:
+        xres['id'] = [t / xdf_input.shape[0] for t in xres['id'].values.tolist()]
+    return xres
+
+
+def num_cat(xdf_input):
+    """ Spalte den Datenbestand vertikal in kategoriale und numerische Teile
+        Beispiel:
+            xdf_num, xdf_cat = dbeschreiben.num_cat(dfres)
+    """
+    xdf_cat = xdf_input.select_dtypes(include=['object']).copy()
+    xdf_num = xdf_input[list(filter(lambda xc: xc not in xdf_cat.columns, xdf_input.columns))]
+    print("\nKategoriale Felder: {}".format(xdf_cat.columns))
+    print("\nNumerische Felder: {}".format(xdf_num.columns))
+    return xdf_num, xdf_cat
+
+
+def kategwert2col(xc):
+    from string import punctuation
+    for c in punctuation:
+        xc = xc.replace(c, "")
+    xc = "".join(list(map(lambda x: x[0].upper() + (x[1:].lower() if len(x) > 1 else ""),
+                          filter(lambda y: y != "",
+                                 map(lambda z: z.strip(), xc.split(" "))
+                                 )
+                          )
+                      )
+                 )
+    return xc
+
+
+def get_dummies(xdf_input, xcol, xkateg_werte):
+    # TODO: spark einsetzen
+    xL = []
+    for ik in range(xdf_input.shape[0]):
+        xdatensatz_wert = xdf_input[xcol].iloc[ik]
+        xL.append(
+            [int(str(t) in str(xdatensatz_wert)) for t in xkateg_werte]
+        )
+    xLdf = pd.DataFrame(xL)
+    xLdf.columns = xkateg_werte
+    return xLdf
+
+
+def kateg2dummy(xdf_input, sep=None):
+    """ Umwandelt die Spalten in Dummy-Variablen mit 1-Hot Encoding
+        Falls in der kategorialen Felder mehrere Werte gleich eingesetzt wurden,
+        dann kann ein Parameter zB sep=";" verwendet werden, um die voneinander zu trennen
+        Beispiel:
+            xdfcat_dummy = dbeschreiben.kateg2dummy(xdf_cat, sep=";")
+    """
+    xL = []
+    xdf = xdf_input.copy()
+    xdf = xdf.reset_index(drop=True)
+
+    def hatsep(xcolumn):
+        if sep is not None:
+            for t in xdf[xcolumn].values:
+                if sep in str(t):
+                    return True
+        return False
+
+    for xcol in xdf.columns:
+        explode = False if sep is None else hatsep(xcol)
+        if not explode:
+            xdfres = pd.get_dummies(xdf[xcol])
+        else:
+            xkateg_werte = list(set(kateg_werte_liste(xdf_input, xcol, sep=sep)))
+            xkateg_werte.sort()
+            xdfres = get_dummies(xdf, xcol, xkateg_werte)
+        xdfres.columns = [xcol + "_" + kategwert2col(xc) for xc in xdfres.columns]
+        xL.append(xdfres)
+    xdfres_gesamt = None
+    for i in range(len(xL) - 1):
+        if xdfres_gesamt is None:
+            xdfres_gesamt = pd.merge(xL[0], xL[1], left_index=True, right_index=True)
+        else:
+            xdfres_gesamt = pd.merge(xdfres_gesamt, xL[i + 1], left_index=True, right_index=True)
+    xdfcat_dummy = xdfres_gesamt
+    xdict_count = {}
+    xLColumns = list(xdfcat_dummy.columns)
+    for i in range(len(xLColumns)):
+        xcol = xLColumns[i]
+        if xcol not in xdict_count.keys():
+            xdict_count[xcol] = 0
+        else:
+            xdict_count[xcol] += 1
+            xLColumns[i] = "{}_{}".format(xLColumns[i], xdict_count[xcol])
+    xdfcat_dummy.columns = xLColumns
+    return xdfcat_dummy
---
+++ notebooks/eda/dersatz.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+
+from sklearn import cluster
+import numpy as np
+import pandas as pd
+
+from eda import dbeschreiben
+
+xfErsatzNullWerteDurchMean = lambda xdf_in: xdf_in.fillna(value=xdf_in.mean())
+xfErsatzNullWerteDurchMedian = lambda xdf_in: xdf_in.fillna(value=xdf_in.median())
+
+
+def datenbestand_spalten(xdf_input, xcol):
+    """ Entsprechend Nullwerte in xcol, spalte den Datenbestand in 2 Gruppen
+    """
+    xdf_temp = xdf_input.copy()
+    xdf_temp[xcol + "_istNull"] = [np.isnan(t) if 'float' in str(type(t)) else False for t in xdf_temp[xcol].values.tolist()]
+    print(xdf_temp[[xcol, xcol + "_istNull"]])
+
+    print(type(xdf_temp[xcol + "_istNull"].values[3]), xdf_temp[xcol + "_istNull"].values[3] is False)
+    xdf_vorhanden = xdf_temp[list(map(lambda x: bool(x) is False, xdf_temp[xcol + "_istNull"].values))]
+    xdf_fehlend = xdf_temp[list(map(lambda x: bool(x) is True, xdf_temp[xcol + "_istNull"].values))]
+
+    print("[x] Unterschied zwischen den zwei Gruppen:")
+
+    print("\n Gruppe ohne fehlenden Daten (Gruppe A) in {}".format(xcol))
+    print(xdf_vorhanden.describe())
+
+    print("\n Gruppe mit fehlenden Daten (Gruppe B) in {}".format(xcol))
+    print(xdf_fehlend.describe())
+
+    print("[x] Prozent der Nullwerte pro Feature:")
+    xcols_cluster = xdf_vorhanden.describe().columns.tolist()
+    xnullwerte_a = [(xc, dbeschreiben.xProzentNullen(xdf_vorhanden, xc)) for xc in xcols_cluster]
+    xnullwerte_a = pd.Series(list(map(lambda x: x[1], xnullwerte_a)),
+                             index=list(map(lambda x: x[0], xnullwerte_a))).to_dict()
+    xnullwerte_b = [(xc, dbeschreiben.xProzentNullen(xdf_fehlend, xc)) for xc in xcols_cluster]
+    xnullwerte_b = pd.Series(list(map(lambda x: x[1], xnullwerte_b)),
+                             index=list(map(lambda x: x[0], xnullwerte_b))).to_dict()
+    print("\n Gruppe A:")
+    print(xnullwerte_a)
+    print("\n Gruppe B:")
+    print(xnullwerte_b)
+    # TODO: zeige noch ein DT, entsprechend binärer Variable
+    return xdf_vorhanden, xdf_fehlend, xnullwerte_a, xnullwerte_b
+
+
+def ersatz_mit_knn(xdf_input, xcol="CareerSatisfaction", ignoriere_spalten=["id"],
+                   threshold_null_werte_cluster=0.7, nclust=3):
+    """ Für jede Zeile, die NAN Werte hat, findet dijenigen Datensätze,
+        die in einem Cluster zusammengehören
+        threshold_null_werte_cluster:
+            Feature für Clustering nicht verwenden, wenn die Nullwerte mehr als so viel Prozent betragen
+
+    """
+    print("=============================================================================================")
+    xdf = xdf_input[list(filter(lambda x: x not in ignoriere_spalten, xdf_input.columns))].copy()
+    if '__cluster__' in xdf.columns:
+        print("[x] Intern wird __cluster__ Feld verwendet!")
+        return
+
+    xdf_vorhanden, xdf_fehlend, xnwa, xnwb = datenbestand_spalten(xdf, xcol)
+    xcols_cluster = xdf_vorhanden.describe().columns.tolist()
+    xcols_cluster = list(filter(lambda x: x != xcol, xcols_cluster))
+    xcols_cluster = list(filter(lambda x: xnwa[x] <= threshold_null_werte_cluster and
+                                          xnwb[x] <= threshold_null_werte_cluster,
+                                xcols_cluster))
+    # xcols_cluster.sort()
+    np.random.seed()
+    np.random.shuffle(xcols_cluster)
+
+    xdf_vorhanden = xfErsatzNullWerteDurchMean(xdf_vorhanden[xcols_cluster])
+    xdf_fehlend = xfErsatzNullWerteDurchMean(xdf_fehlend[xcols_cluster])
+
+    # TODO: Übergabe an xKMeans
+    anzahl_cluster = nclust
+    clf = cluster.KMeans(n_clusters=anzahl_cluster)  # random_state=42
+    clf.fit(xdf_vorhanden[xcols_cluster].values)
+    xdf_vorhanden['__cluster__'] = clf.labels_
+
+    print("\n[x] Beispiel Resultat:")
+    print(xdf_vorhanden[xcols_cluster + ["__cluster__"]].sample(20))
+    xdf_clustering_data = xfErsatzNullWerteDurchMean(xdf[xcols_cluster])
+    xdf["__cluster__"] = clf.predict(xdf_clustering_data)
+
+    print("\n[x] Datensätze klassifiziert:")
+    print(xdf[[xcol] + xcols_cluster + ['__cluster__']])
+
+    print("\n[x] Frequenz der Datensätze im jeweiligen Cluster:")
+    _ = dbeschreiben.frequenz_werte(xdf, xcol="__cluster__", prozente=True)
+
+    cluster_means = xdf.groupby("__cluster__").mean()[xcol].to_dict()
+    print("\n[x] Durchschnitte im Cluster: ", cluster_means)
+
+    xgruppen = []
+    for cl in range(anzahl_cluster):
+        xgruppen.append(
+            xdf[xdf["__cluster__"] == cl].fillna(value={xcol: cluster_means[cl]})
+        )
+    xdf_res = pd.concat(xgruppen)
+
+    print("\n [x] Wenn alles gut gelaufen ist, haben sich die statistischen Maßen nicht wesentlich geändert:")
+    print("\t VORHER:")
+    print(xdf_input.describe())
+    print("\n\t NACHHER:")
+    print(xdf_res.describe())
+
+    xdf_res = xdf_res[list(filter(lambda x: x != '__cluster__' and x != (xcol + "_istNull"), xdf_res.columns))]
+    for xc in ignoriere_spalten:
+        xdf_res[xc] = xdf_input[xc]
+
+    print("=============================================================================================")
+    return xdf_res, clf
